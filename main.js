9e65e4aa5427c816b2c52dd91d86f250b63c31ed
boedegoat <bhremada.fka@gmail.com>
2023-12-16 10:41:56 +0700

    create contact us section

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const navMenuBtn = document.querySelector('.nav-mobile-menu-btn')
 const navCloseBtn = document.querySelector('.nav-mobile-close-btn')
 const navLinks = document.querySelector('.nav-links')
+const navLinkItems = document.querySelectorAll('.nav-link')
 
 navMenuBtn.addEventListener('click', () => {
     navLinks.classList.add('open')
@@ -10,4 +11,11 @@ navMenuBtn.addEventListener('click', () => {
 navCloseBtn.addEventListener('click', () => {
     navLinks.classList.remove('open')
     document.documentElement.style.overflow = 'auto'
+})
+
+navLinkItems.forEach((link) => {
+    link.addEventListener('click', () => {
+        navLinks.classList.remove('open')
+        document.documentElement.style.overflow = 'auto'
+    })
 })
